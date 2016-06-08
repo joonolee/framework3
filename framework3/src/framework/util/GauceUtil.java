173fcdc5f2f577a9b893d4872ca7ae0d18e6b7b5
jhlee <jhlee@jhlee-Desktop>
2016-06-08 11:49:12 +0900

    한글처리 부분 방법 변경 *2 제거

--- framework3/src/framework/util/GauceUtil.java
+++ framework3/src/framework/util/GauceUtil.java
@@ -433,8 +433,7 @@ public class GauceUtil {
 					}
 					dSet.put(colNms[c], rs.getDouble(colNms[c]), dblSize, GauceDataColumn.TB_DECIMAL);
 				} else {
-					// 한글깨짐으로 인해 colSize[c] 에 2를 곱하여 해결
-					dSet.put(colNms[c], (rs.getString(colNms[c])), colSize[c] * 2, GauceDataColumn.TB_NORMAL);
+					dSet.put(colNms[c], (rs.getString(colNms[c])), colSize[c], GauceDataColumn.TB_NORMAL);
 				}
 			}
 		}
@@ -465,8 +464,7 @@ public class GauceUtil {
 						}
 						dSet.put(colNms[c], rs.getDouble(colNms[c]), dblSize, GauceDataColumn.TB_DECIMAL);
 					} else {
-						// 한글깨짐으로 인해 colSize[c] 에 2를 곱하여 해결
-						dSet.put(colNms[c], (rs.getString(colNms[c])), colSize[c] * 2, GauceDataColumn.TB_NORMAL);
+						dSet.put(colNms[c], (rs.getString(colNms[c])), colSize[c], GauceDataColumn.TB_NORMAL);
 					}
 				}
 			}
