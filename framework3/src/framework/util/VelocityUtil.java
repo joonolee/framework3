04b894a1e097c97af7963c8770e441ac82465582
JunHo Lee <joono_lee@naver.com>
2017-12-11 23:28:59 +0900

    VelocityUtil 기능 개선

--- framework3/src/framework/util/VelocityUtil.java
+++ framework3/src/framework/util/VelocityUtil.java
@@ -7,14 +7,12 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.ResourceBundle;
 
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletContext;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
-
-import framework.action.Params;
 
 /**
  * Velocity를 이용한 템플릿 처리 라이브러리
@@ -29,17 +27,17 @@ public class VelocityUtil {
 	}
 
 	/**
-	 * views.properties 파일에 설정된 key와 연결된 템플릿 파일에서 statement에 정의된 COMMAND의 문자열을 파라미터를 
+	 * views.properties 파일에 설정된 key와 연결된 템플릿 파일에서 statement에 정의된 COMMAND의 문자열을 파라미터를
 	 * 적용한 문자열을 생성한다.
 	 * <br>
 	 * Sql 문장생성 및 이메일 발송을 위한 템플릿 생성할때 응용할 수 있다.
-	 * @param servlet 서블릿 객체
-	 * @param key views.properties에 등록한 템플릿의 키 문자열 
+	 * @param sc 서블릿 컨텍스트 객체
+	 * @param key views.properties에 등록한 템플릿의 키 문자열
 	 * @param statement 문장식별 문자열
-	 * @param param 파라미터 Params 객체
+	 * @param param 파라미터 객체
 	 * @return 템플릿이 적용된 문자열
 	 */
-	public static String render(HttpServlet servlet, String key, String statement, Params param) {
+	public static String render(ServletContext sc, String key, String statement, Object param) {
 		StringWriter writer = new StringWriter();
 		try {
 			Velocity.init();
@@ -47,9 +45,9 @@ public class VelocityUtil {
 			context.put("COMMAND", statement);
 			context.put("PARAM", param);
 			context.put("UTIL", StringUtil.class);
-			ResourceBundle viewsBundle = (ResourceBundle) servlet.getServletContext().getAttribute("views-mapping");
+			ResourceBundle viewsBundle = (ResourceBundle) sc.getAttribute("views-mapping");
 			String fileName = ((String) viewsBundle.getObject(key)).trim();
-			String template = readTemplate(servlet, fileName);
+			String template = readTemplate(sc, fileName);
 			StringReader reader = new StringReader(template);
 			Velocity.evaluate(context, writer, "framework.util.VelocityUtil", reader);
 		} catch (Throwable e) {
@@ -61,13 +59,13 @@ public class VelocityUtil {
 	/**
 	 * 템플릿파일을 읽어들인다.
 	 */
-	private static String readTemplate(HttpServlet servlet, String fileName) {
-		String pathFile = servlet.getServletContext().getRealPath(fileName);
+	private static String readTemplate(ServletContext sc, String fileName) {
+		String pathFile = sc.getRealPath(fileName);
 		return read(pathFile);
 	}
 
-	/** 
-	 * 파일의 path의 파일 내용 읽어서 String으로 리턴한다 
+	/**
+	 * 파일의 path의 파일 내용 읽어서 String으로 리턴한다
 	 */
 	private static String read(String pathFile) {
 		StringBuilder ta = new StringBuilder();
