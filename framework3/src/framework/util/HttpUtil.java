c1c16e383d55a2f51829025c3f2ce5b59b52486a
JunHo Lee <joono_lee@naver.com>
2019-05-04 11:13:58 +0900

    refactoring

--- framework3/src/framework/util/HttpUtil.java
+++ framework3/src/framework/util/HttpUtil.java
@@ -28,6 +28,7 @@ import org.apache.http.util.EntityUtils;
  * HTTP 클라이언트의 기능을 이용할 수 있는 유틸리티 클래스
  */
 public final class HttpUtil {
+	private static final String DEFAULT_CONTENT_TYPE = "application/x-www-form-urlencoded";
 
 	/**
 	 * 생성자, 외부에서 객체를 인스턴스화 할 수 없도록 설정
@@ -135,7 +136,7 @@ public final class HttpUtil {
 	 * @return Result 객체
 	 */
 	public static Result post(String url) {
-		return post(url, (String) null, (Map<String, String>) null);
+		return post(url, (Map<String, String>) null, (Map<String, String>) null);
 	}
 
 	/**
@@ -145,7 +146,7 @@ public final class HttpUtil {
 	 * @return Result 객체
 	 */
 	public static Result post(String url, int timeoutMilliseconds) {
-		return post(url, (String) null, (Map<String, String>) null, timeoutMilliseconds);
+		return post(url, (Map<String, String>) null, (Map<String, String>) null, timeoutMilliseconds);
 	}
 
 	/**
@@ -167,27 +168,6 @@ public final class HttpUtil {
 	 */
 	public static Result post(String url, Map<String, String> paramMap, int timeoutMilliseconds) {
 		return post(url, paramMap, (Map<String, String>) null, timeoutMilliseconds);
-	}
-
-	/**
-	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
-	 * @param url url 주소
-	 * @param paramStr 파라미터 문자열
-	 * @return Result 객체
-	 */
-	public static Result post(String url, String paramStr) {
-		return post(url, paramStr, (Map<String, String>) null);
-	}
-
-	/**
-	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
-	 * @param url url 주소
-	 * @param paramStr 파라미터 문자열
-	 * @param timeoutMilliseconds 소켓 타임아웃 시간(밀리세컨드)
-	 * @return Result 객체
-	 */
-	public static Result post(String url, String paramStr, int timeoutMilliseconds) {
-		return post(url, paramStr, (Map<String, String>) null, timeoutMilliseconds);
 	}
 
 	/**
@@ -230,60 +210,6 @@ public final class HttpUtil {
 					params.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
 				}
 				UrlEncodedFormEntity ent = new UrlEncodedFormEntity(params, "UTF-8");
-				httpPost.setEntity(ent);
-			}
-			HttpResponse response = httpClient.execute(httpPost);
-			statusCode = response.getStatusLine().getStatusCode();
-			HttpEntity resEntity = response.getEntity();
-			if (resEntity != null) {
-				content = EntityUtils.toString(resEntity);
-			}
-		} catch (Throwable e) {
-			throw new RuntimeException(e);
-		} finally {
-			if (httpClient != null) {
-				httpClient.getConnectionManager().shutdown();
-			}
-		}
-		return new Result(statusCode, content);
-	}
-
-	/**
-	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
-	 * @param url url 주소
-	 * @param paramStr 파라미터 문자열
-	 * @param headerMap 헤더 맵 객체
-	 * @return Result 객체
-	 */
-	public static Result post(String url, String paramStr, Map<String, String> headerMap) {
-		return post(url, paramStr, headerMap, 0);
-	}
-
-	/**
-	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
-	 * @param url url 주소
-	 * @param paramStr 파라미터 문자열
-	 * @param headerMap 헤더 맵 객체
-	 * @param timeoutMilliseconds 소켓 타임아웃 시간(밀리세컨드)
-	 * @return Result 객체
-	 */
-	public static Result post(String url, String paramStr, Map<String, String> headerMap, int timeoutMilliseconds) {
-		int statusCode = 0;
-		String content = "";
-		HttpClient httpClient = null;
-		try {
-			httpClient = new DefaultHttpClient();
-			if (timeoutMilliseconds > 0) {
-				httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, timeoutMilliseconds);
-			}
-			HttpPost httpPost = new HttpPost(url);
-			if (headerMap != null) {
-				for (Entry<String, String> entry : headerMap.entrySet()) {
-					httpPost.addHeader(entry.getKey(), entry.getValue());
-				}
-			}
-			if (paramStr != null) {
-				StringEntity ent = new StringEntity(paramStr, "UTF-8");
 				httpPost.setEntity(ent);
 			}
 			HttpResponse response = httpClient.execute(httpPost);
@@ -374,6 +300,132 @@ public final class HttpUtil {
 				}
 			}
 			httpPost.setEntity(reqEntity);
+			HttpResponse response = httpClient.execute(httpPost);
+			statusCode = response.getStatusLine().getStatusCode();
+			HttpEntity resEntity = response.getEntity();
+			if (resEntity != null) {
+				content = EntityUtils.toString(resEntity);
+			}
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (httpClient != null) {
+				httpClient.getConnectionManager().shutdown();
+			}
+		}
+		return new Result(statusCode, content);
+	}
+
+	/**
+	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
+	 * @param url url 주소
+	 * @param paramStr 파라미터 문자열(컨텐트 타입: application/x-www-form-urlencoded)
+	 * @return Result 객체
+	 */
+	public static Result post(String url, String paramStr) {
+		return post(url, paramStr, DEFAULT_CONTENT_TYPE, (Map<String, String>) null);
+	}
+
+	/**
+	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
+	 * @param url url 주소
+	 * @param paramStr 파라미터 문자열
+	 * @param contentType 파라미터 컨텐트 타입(예: application/x-www-form-urlencoded, application/json)
+	 * @return Result 객체
+	 */
+	public static Result post(String url, String paramStr, String contentType) {
+		return post(url, paramStr, contentType, (Map<String, String>) null);
+	}
+
+	/**
+	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
+	 * @param url url 주소
+	 * @param paramStr 파라미터 문자열
+	 * @param timeoutMilliseconds 소켓 타임아웃 시간(밀리세컨드)
+	 * @return Result 객체
+	 */
+	public static Result post(String url, String paramStr, int timeoutMilliseconds) {
+		return post(url, paramStr, DEFAULT_CONTENT_TYPE, (Map<String, String>) null, timeoutMilliseconds);
+	}
+
+	/**
+	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
+	 * @param url url 주소
+	 * @param paramStr 파라미터 문자열
+	 * @param contentType 파라미터 컨텐트 타입(예: application/x-www-form-urlencoded, application/json)
+	 * @param timeoutMilliseconds 소켓 타임아웃 시간(밀리세컨드)
+	 * @return Result 객체
+	 */
+	public static Result post(String url, String paramStr, String contentType, int timeoutMilliseconds) {
+		return post(url, paramStr, contentType, (Map<String, String>) null, timeoutMilliseconds);
+	}
+
+	/**
+	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
+	 * @param url url 주소
+	 * @param paramStr 파라미터 문자열
+	 * @param headerMap 헤더 맵 객체
+	 * @return Result 객체
+	 */
+	public static Result post(String url, String paramStr, Map<String, String> headerMap) {
+		return post(url, paramStr, null, headerMap, 0);
+	}
+
+	/**
+	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
+	 * @param url url 주소
+	 * @param paramStr 파라미터 문자열
+	 * @param contentType 파라미터 컨텐트 타입(예: application/x-www-form-urlencoded, application/json)
+	 * @param headerMap 헤더 맵 객체
+	 * @return Result 객체
+	 */
+	public static Result post(String url, String paramStr, String contentType, Map<String, String> headerMap) {
+		return post(url, paramStr, contentType, headerMap, 0);
+	}
+
+	/**
+	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
+	 * @param url url 주소
+	 * @param paramStr 파라미터 문자열
+	 * @param headerMap 헤더 맵 객체
+	 * @param timeoutMilliseconds 소켓 타임아웃 시간(밀리세컨드)
+	 * @return Result 객체
+	 */
+	public static Result post(String url, String paramStr, Map<String, String> headerMap, int timeoutMilliseconds) {
+		return post(url, paramStr, null, headerMap, timeoutMilliseconds);
+	}
+
+	/**
+	 * url 을 Post 방식으로 호출하고 결과를 리턴한다.
+	 * @param url url 주소
+	 * @param paramStr 파라미터 문자열
+	 * @param contentType 파라미터 컨텐트 타입(예: application/x-www-form-urlencoded, application/json)
+	 * @param headerMap 헤더 맵 객체
+	 * @param timeoutMilliseconds 소켓 타임아웃 시간(밀리세컨드)
+	 * @return Result 객체
+	 */
+	public static Result post(String url, String paramStr, String contentType, Map<String, String> headerMap, int timeoutMilliseconds) {
+		int statusCode = 0;
+		String content = "";
+		HttpClient httpClient = null;
+		try {
+			httpClient = new DefaultHttpClient();
+			if (timeoutMilliseconds > 0) {
+				httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, timeoutMilliseconds);
+			}
+			HttpPost httpPost = new HttpPost(url);
+			if (headerMap != null) {
+				for (Entry<String, String> entry : headerMap.entrySet()) {
+					httpPost.addHeader(entry.getKey(), entry.getValue());
+				}
+			}
+			if (paramStr != null) {
+				StringEntity ent = new StringEntity(paramStr, "UTF-8");
+				if (contentType != null) {
+					ent.setContentType(contentType);
+				}
+				httpPost.setEntity(ent);
+			}
 			HttpResponse response = httpClient.execute(httpPost);
 			statusCode = response.getStatusLine().getStatusCode();
 			HttpEntity resEntity = response.getEntity();
