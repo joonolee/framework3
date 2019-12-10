beca376275f4584c63daf4026f8c0b705df97905
JunHo Lee <joono_lee@naver.com>
2019-12-10 21:28:43 +0900

    프로젝트명 변경: daogen3 to codegen3

--- daogen3/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set XGENCLASSPATH=lib\w3c.jar;lib\xalan-2.0.0.jar;lib\xalanj1compat.jar;lib\xerces-1.2.3.jar;lib
-
-mkdir build
-mkdir build\vo
-java -classpath %XGENCLASSPATH% XtSax xml\%1.xml xsl\vo.xsl  > build\vo\%1VO.java
-java -classpath %XGENCLASSPATH% XtSax xml\%1.xml xsl\dao.xsl  > build\vo\%1DAO.java
